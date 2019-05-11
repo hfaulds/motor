563e0f8c2d115adc5da0dbdffe77d2b4a00ad676
Hayden Faulds <fauldsh@gmail.com>
2019-05-11 18:05:40 +0100

    attempt at buttons implementation

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use std::time::Duration;
 use rppal::gpio::Error;
 use rppal::gpio::Gpio;
 use rppal::gpio::Level;
+use rppal::gpio::OutputPin;
 
-const BCM_PINS: [u8; 4] = [4, 17, 27, 22];
+const MOTOR_BCM_PINS: [u8; 4] = [4, 17, 27, 22];
+const BUTTON_UP_BCM_PIN: u8 = 23;
+const BUTTON_DOWN_BCM_PIN: u8 = 24;
 
 const HALFSTEP_SEQ: [[Level; 4]; 8] = [
     [Level::High, Level::Low, Level::Low, Level::Low],
@@ -18,28 +21,72 @@ const HALFSTEP_SEQ: [[Level; 4]; 8] = [
     [Level::High, Level::Low, Level::Low, Level::High],
 ];
 
+enum Direction {
+    NoDirection,
+    Up,
+    Down,
+}
+
 fn main() -> Result<(), Error> {
     let gpio = match Gpio::new() {
         Ok(g) => g,
         Err(e) => return Err(e),
     };
 
+    let mut motor_pins = match create_motor_pins(&gpio) {
+        Ok(p) => p,
+        Err(e) => return Err(e),
+    };
+    let button_up_pin = match gpio.get(BUTTON_UP_BCM_PIN) {
+        Ok(p) => p.into_input(),
+        Err(e) => return Err(e),
+    };
+    let button_down_pin = match gpio.get(BUTTON_DOWN_BCM_PIN) {
+        Ok(p) => p.into_input(),
+        Err(e) => return Err(e),
+    };
+
+    loop {
+        let mut direction = Direction::NoDirection;
+        if button_up_pin.is_high() {
+            direction = Direction::Up;
+        }
+        if button_down_pin.is_high() {
+            direction = Direction::Down;
+        }
+        match direction {
+            Direction::NoDirection => {
+                thread::sleep(Duration::from_millis(1));
+                continue;
+            },
+            Direction::Up => {
+                for step in HALFSTEP_SEQ.iter() {
+                    move_motor(&mut motor_pins, *step)
+                }
+            },
+            Direction::Down => {
+                for step in HALFSTEP_SEQ.iter().rev() {
+                    move_motor(&mut motor_pins, *step)
+                }
+            },
+        };
+    }
+}
+
+fn create_motor_pins(gpio: &Gpio) -> Result<Vec<OutputPin>, Error> {
     let mut pins = Vec::new();
-    for bcm_pin in BCM_PINS.into_iter() {
+    for bcm_pin in MOTOR_BCM_PINS.into_iter() {
         match gpio.get(*bcm_pin) {
             Ok(p) => pins.push(p.into_output()),
             Err(e) => return Err(e),
         }
     }
+    return Ok(pins);
+}
 
-    for _ in 0..512 {
-        for halfstep in HALFSTEP_SEQ.iter() {
-            for (pin_index, level) in halfstep.iter().enumerate() {
-                pins[pin_index].write(*level)
-            }
-            thread::sleep(Duration::from_millis(1));
-        }
+fn move_motor(motor_pins: &mut Vec<OutputPin>, step: [Level; 4]) {
+    for (pin_index, level) in step.iter().enumerate() {
+        motor_pins[pin_index].write(*level);
     }
-
-    return Ok(());
+    thread::sleep(Duration::from_millis(1));
 }
